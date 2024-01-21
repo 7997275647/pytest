168fc8202abe00c9b564252f7e5c65d27cc18c90
Sandeep Reddy Modugu <modugusandy@gmail.com>
2024-01-21 19:57:32 +0100

    parameterization

---
+++ SkipMark/test_parametrize.py
@@ -0,0 +1,19 @@
+import pytest
+import math
+@pytest.mark.parametrize("input_value",[89,34,76,65])
+def test_parameter01(input_value):
+    assert input_value > 50
+@pytest.mark.parametrize("input,output",[(2,8),(3,27),(4,64)])
+def test_parameter02(input,output):
+    assert input**3 == output
+data = [
+    ([1,2,3],"sum",6),([1,2,3],"prod",6)
+]
+@pytest.mark.parametrize("a,b,c",data)
+def test_parameter03(a,b,c):
+    if b== "sum":
+        assert sum(a) ==6
+    elif b=="prod":
+        assert math.prod(a)==6
+
+
